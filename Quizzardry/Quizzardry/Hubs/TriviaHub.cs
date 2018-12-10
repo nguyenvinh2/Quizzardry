bf023bafd019c811de718ed20fc9a7322658b07d
nguye <nguyenv2@outlook.com>
2018-12-10 11:14:43 -0800

    generate first home page and created hub

---
+++ Quizzardry/Quizzardry/Hubs/TriviaHub.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.SignalR;
+using Microsoft.AspNetCore.Http;
+
+namespace Quizzardry.Hubs
+{
+    public class TriviaHub:Hub
+    {
+
+        public async Task SendMessage(string user, IFormCollection option)
+        {
+            await Clients.All.SendAsync("ReceiveMessage", option["user"], option["option"]);
+        }
+
+    }
+}
--- Quizzardry/Quizzardry/Startup.cs
+++ Quizzardry/Quizzardry/Startup.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace Quizzardry
@@ -15,6 +16,16 @@ namespace Quizzardry
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.Configure<CookiePolicyOptions>(options =>
+            {
+                // This lambda determines whether user consent for non-essential cookies is needed for a given request.
+                options.CheckConsentNeeded = context => true;
+                options.MinimumSameSitePolicy = SameSiteMode.None;
+            });
+
+
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            services.AddSignalR();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -24,11 +35,28 @@ namespace Quizzardry
             {
                 app.UseDeveloperExceptionPage();
             }
-
-            app.Run(async (context) =>
+            else
             {
-                await context.Response.WriteAsync("Hello World!");
+                app.UseExceptionHandler("/Home/Error");
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+            app.UseCookiePolicy();
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                name: "default",
+                template: "{Controller=Index}");
             });
+
+            //app.UseSignalR(routes =>
+            //{
+            //    routes.MapHub<TriviaHub>("/triviaHub");
+            //});
+
+            app.UseMvc();
         }
     }
 }
