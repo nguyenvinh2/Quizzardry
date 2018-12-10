e60fa28a633babcd471676f546f992720d10c228
nguye <nguyenv2@outlook.com>
2018-12-10 11:34:40 -0800

    commit for pulling down stagin

--- Quizzardry/Quizzardry/Hubs/TriviaHub.cs
+++ Quizzardry/Quizzardry/Hubs/TriviaHub.cs
@@ -4,15 +4,16 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.SignalR;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Quizzardry.Hubs
 {
     public class TriviaHub:Hub
     {
 
-        public async Task SendMessage(string user, IFormCollection option)
+        public async Task<IActionResult> SendMessage(string user)
         {
-            await Clients.All.SendAsync("ReceiveMessage", option["user"], option["option"]);
+            await Clients.All.SendAsync("ReceiveMessage", user);
         }
 
     }
---
+++ Quizzardry/Quizzardry/wwwroot/js/chat.js
@@ -0,0 +1,24 @@
+﻿"use strict";
+
+var connection = new signalR.HubConnectionBuilder().withUrl("/triviaHub").build();
+
+connection.on("ReceiveMessage", function (user, option) {
+  var msg = option.replace(/&/g, "&amp;").replace(/</g, "&lt;").replace(/>/g, "&gt;");
+  var encodedMsg = user + " says " + msg;
+  var li = document.createElement("li");
+  li.textContent = encodedMsg;
+  document.getElementById("messagesList").appendChild(li);
+});
+
+connection.start().catch(function (err) {
+  return console.error(err.toString());
+});
+
+document.getElementById("sendButton").addEventListener("click", function (event) {
+  var user = document.getElementById("userInput").value;
+  var option = document.getElementsByName("option").values;
+  connection.invoke("SendMessage", user, option).catch(function (err) {
+    return console.error(err.toString());
+  });
+  event.preventDefault();
+});
