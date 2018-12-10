199b510769f01ba7a5955f63ebe08551d35c4600
jimmyn123 <jimmyn123@gmail.com>
2018-12-10 11:25:09 -0800

    Revert "Ay ay jim day1"

--- Quizzardry/Quizzardry/Data/QuestionsDbContext.cs
+++ Quizzardry/Quizzardry/Data/QuestionsDbContext.cs
@@ -1,42 +1,11 @@
-﻿using Microsoft.EntityFrameworkCore;
-using Quizzardry.Models;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace Quizzardry.Data
 {
-    public class QuestionsDbContext : DbContext
+    public class QuestionsDbContext
     {
-        public QuestionsDbContext(DbContextOptions<QuestionsDbContext> options) : base(options)
-        {
-
-        }
-
-        protected override void OnModelCreating(ModelBuilder mb)
-        {
-            mb.Entity<Questions>().HasData(
-                new Questions
-                {
-                    ID = 1,
-                    Question = "Who is Neth's favorite artist?",
-                    Answer1 = "REM",
-                    Answer2 = "Drake",
-                    Answer3 = "Busta Rhymes",
-                    CorrectAnswer = "Taylor Swift"
-                },
-
-                new Questions
-                {
-                    ID = 2,
-                    Question = "Who is Vinh's favorite person?",
-                    Answer1 = "Suzy",
-                    Answer2 = "Amanda",
-                    Answer3 = "Kendra",
-                    CorrectAnswer = "TRE"
-                }
-            );
-        }
     }
 }
--- Quizzardry/Quizzardry/Models/Users.cs
+++ Quizzardry/Quizzardry/Models/Users.cs
@@ -7,6 +7,5 @@ namespace Quizzardry.Models
 {
     public class Users
     {
-
     }
 }
