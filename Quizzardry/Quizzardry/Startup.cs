5f4aaf2a4ff90ec93b9d8e3af7df74c96d03e710
nguye <nguyenv2@outlook.com>
2018-12-10 16:00:05 -0800

    commit after merge

--- Quizzardry/Quizzardry/Startup.cs
+++ Quizzardry/Quizzardry/Startup.cs
@@ -57,25 +57,6 @@ namespace Quizzardry
 
             services.AddScoped<IPlayer, PlayerService>();
         }
-
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
-        {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-
-            app.Run(async (context) =>
-            {
-                await context.Response.WriteAsync("Hello World!");
-            });
-        }
-    }
-}
-            });
-        }
-
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
