5eb09a22185b3ca27f922cbb28b6fe4fc317d1ee
Aaron Frank <aaron.frank41@gmail.com>
2018-12-12 10:57:03 -0800

    can run through first three questions

--- Quizzardry/Quizzardry/wwwroot/js/chat.js
+++ Quizzardry/Quizzardry/wwwroot/js/chat.js
@@ -1,69 +1,75 @@
 ﻿"use strict";
 
 var connection = new signalR.HubConnectionBuilder().withUrl("/triviaHub").build();
-var count = 0;
+var round = 0;
 
-connection.start().then(function () {
-    var user = document.getElementById("userInput").value;
-    var userGuid = document.getElementById("userInputGuid").value;
-    connection.invoke("SendUser", user, userGuid).catch(function (err) {
+$(document).ready(function () {
+    $(".questions").hide();
+    $("#question1").show();
+
+
+    connection.start().then(function () {
+        var user = document.getElementById("userInput").value;
+        var userGuid = document.getElementById("userInputGuid").value;
+        connection.invoke("SendUser", user, userGuid).catch(function (err) {
+            return console.error(err.toString());
+        });
+    }).catch(function (err) {
         return console.error(err.toString());
     });
-}).catch(function (err) {
-    return console.error(err.toString());
-});
 
-connection.on("ReceiveUser", function (userList) {
-    var users = document.getElementById("userList");
-    while (users.firstChild) {
-        users.removeChild(users.firstChild);
-    }
-    for (let i = 0; i < userList.length; i++) {
-        var encodedMsg = userList[i].name + " has joined the game." + "Score is " + userList[i].score;
-        console.log(userList[i]);
+    connection.on("ReceiveUser", function (userList) {
+        var users = document.getElementById("userList");
+        while (users.firstChild) {
+            users.removeChild(users.firstChild);
+        }
+        for (let i = 0; i < userList.length; i++) {
+            var encodedMsg = userList[i].name + " has joined the game." + "Score is " + userList[i].score;
+            console.log(userList[i]);
+            var li = document.createElement("li");
+            li.textContent = encodedMsg;
+            document.getElementById("userList").appendChild(li);
+        }
+        round++;
+        var currentQuestionId = "question" + round;
+        $(".questions").hide();
+        $(`#${currentQuestionId}`).show();
+    });
+
+    connection.on("ReceiveMessage", function (user, message) {
+        var msg = message.replace(/&/g, "&amp;").replace(/</g, "&lt;").replace(/>/g, "&gt;");
+        var encodedMsg = user + " says " + msg;
         var li = document.createElement("li");
         li.textContent = encodedMsg;
-        document.getElementById("userList").appendChild(li);
-        if (userList[i].isAdmin) {
-            document.getElementById("resultButton").classList.remove("removeMe");
-        } else {
-            document.getElementById("resultButton").classList.add("removeMe");
-        }
-    }
-});
-
-connection.on("ReceiveMessage", function (user, message) {
-    var msg = message.replace(/&/g, "&amp;").replace(/</g, "&lt;").replace(/>/g, "&gt;");
-    var encodedMsg = user + " says " + msg;
-    var li = document.createElement("li");
-    li.textContent = encodedMsg;
-    document.getElementById("messagesList").appendChild(li);
-});
-
-document.getElementById("sendButton").addEventListener("click", function (event) {
-    var user = document.getElementById("userInput").value;
-    var message = document.getElementById("messageInput").value;
-    connection.invoke("SendMessage", user, message).catch(function (err) {
-        return console.error(err.toString());
+        document.getElementById("messagesList").appendChild(li);
     });
-    event.preventDefault();
-});
 
-$("#voteButton").click(function () {
-  var $answer = $('input[name=answer-options]:checked').val();
-  var userGuid = document.getElementById("userInputGuid").value;
-  connection.invoke("AddPoints", userGuid, $answer).catch(function (err) {
-    return console.error(err.toString());
-  });
-  event.preventDefault();
-});
+    document.getElementById("sendButton").addEventListener("click", function (event) {
+        var user = document.getElementById("userInput").value;
+        var message = document.getElementById("messageInput").value;
+        connection.invoke("SendMessage", user, message).catch(function (err) {
+            return console.error(err.toString());
+        });
+        event.preventDefault();
+    });
+
+    $(".voteButton").click(function () {
+      var $answer = $('input[name=answer-options]:checked').val();
+      var userGuid = document.getElementById("userInputGuid").value;
+      connection.invoke("AddPoints", userGuid, $answer).catch(function (err) {
+        return console.error(err.toString());
+      });
+      event.preventDefault();
+    });
 
 
-$("#resultButton").click(function () {
-  var user = document.getElementById("userInput").value;
-  var userGuid = document.getElementById("userInputGuid").value;
-  connection.invoke("SubmitAnswers").catch(function (err) {
-    return console.error(err.toString());
-  });
-  event.preventDefault();
+    $(".submitButton").click(function () {
+      var user = document.getElementById("userInput").value;
+      var userGuid = document.getElementById("userInputGuid").value;
+      connection.invoke("SubmitAnswers").catch(function (err) {
+        return console.error(err.toString());
+      });
+        event.preventDefault();
+    });
+
 });
