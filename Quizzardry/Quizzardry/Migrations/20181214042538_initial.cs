2641dbb07ff766ca057b0bbe605c98a6844be27f
Jimmy Nguyen <jimmyn123@gmail.com>
2018-12-13 20:29:06 -0800

    deleted migrations and added new one

--- Quizzardry/Quizzardry/Migrations/20181214042538_initial.cs
+++ Quizzardry/Quizzardry/Migrations/20181214042538_initial.cs
@@ -35,7 +35,17 @@ namespace Quizzardry.Migrations
                     { 3, "The Ronnettes", "The Supremes", "The Byrds", "The Shirellas", "b", "Covered by Phil Collins in 1982, what group released \"You Can't Hurry Love\" in 1966?" },
                     { 4, "George Takei", "James Doohan", "DeForest Kelley", "Leonard Nemoy", "a", "Which Star Trek TOS cast member never appeared in an epsiode of Star Trek: TNG?" },
                     { 5, "The Space Needle", "The Statue of Liberty", "The Washington Monument", "The Gateway Arch", "d", "Which of the following structures is the tallest?" },
-                    { 6, "Pong", "Space Invaders", "Computer Space", "Asteroids", "c", "What was the name of the first commercially released video game?" }
+                    { 6, "Pong", "Space Invaders", "Computer Space", "Asteroids", "c", "What was the name of the first commercially released video game?" },
+                    { 7, "Fourteen", "Fifteen", "Two hundred and sixty seven", "Sixteen", "b", "How many broomsticks are flown in a full game of Quidditch?" },
+                    { 8, "Liquorice Wand", "Sherbert Lemon", "Fizzing Whizzbee", "Cockroach Cluster", "a", "Which is not a password to get into Dumbledor's Office?" },
+                    { 9, "Hufflepuff", "Gryffindor", "Slytherin", "Ravenclaw", "d", "Which Hogwarts house is Terry Boot a member of?" },
+                    { 10, "Fluxweed", "Newt spleen", "A bit of whoever you wish toturn into", "Knotgrass", "a", "Which Polyjuice Potion ingredient must be acquired at the full moon?" },
+                    { 11, "Bird", "Serpent", "Fish", "Dog", "b", "What type of creature is an Ashwinder?" },
+                    { 12, "Lou Gehrig", "Pete Rose", "Cal Ripken Jr.", "Stan Musial", "c", "Which player holds the record for most consecutive games played in the MLB?" },
+                    { 13, "Indiana Pacers", "Mulwaukee Bucks", "Houston Rockets", "New York Knicks", "a", "Which of these teams has never won a championship?" },
+                    { 14, "Montreal Canadiens", "Green Bay Packers", "Boston Celtics", "New York Yankees", "d", "Which Big 4 team has the most championship wins with 27?" },
+                    { 15, "Hulk Hogan", "Andre the Giant", "Randy Savage", "Vince McMahon", "b", "Who was the first person inducted into the WWE Hall of Fame?" },
+                    { 16, "Pack-12", "SEC", "Big 10", "ACC", "d", "Which conference includes the Duke Blue Devils?" }
                 });
         }
 
--- Quizzardry/Quizzardry/Migrations/QuestionsDbContextModelSnapshot.cs
+++ Quizzardry/Quizzardry/Migrations/QuestionsDbContextModelSnapshot.cs
@@ -52,7 +52,17 @@ namespace Quizzardry.Migrations
                         new { ID = 3, Answer1 = "The Ronnettes", Answer2 = "The Supremes", Answer3 = "The Byrds", Answer4 = "The Shirellas", CorrectAnswer = "b", Question = "Covered by Phil Collins in 1982, what group released \"You Can't Hurry Love\" in 1966?" },
                         new { ID = 4, Answer1 = "George Takei", Answer2 = "James Doohan", Answer3 = "DeForest Kelley", Answer4 = "Leonard Nemoy", CorrectAnswer = "a", Question = "Which Star Trek TOS cast member never appeared in an epsiode of Star Trek: TNG?" },
                         new { ID = 5, Answer1 = "The Space Needle", Answer2 = "The Statue of Liberty", Answer3 = "The Washington Monument", Answer4 = "The Gateway Arch", CorrectAnswer = "d", Question = "Which of the following structures is the tallest?" },
-                        new { ID = 6, Answer1 = "Pong", Answer2 = "Space Invaders", Answer3 = "Computer Space", Answer4 = "Asteroids", CorrectAnswer = "c", Question = "What was the name of the first commercially released video game?" }
+                        new { ID = 6, Answer1 = "Pong", Answer2 = "Space Invaders", Answer3 = "Computer Space", Answer4 = "Asteroids", CorrectAnswer = "c", Question = "What was the name of the first commercially released video game?" },
+                        new { ID = 7, Answer1 = "Fourteen", Answer2 = "Fifteen", Answer3 = "Two hundred and sixty seven", Answer4 = "Sixteen", CorrectAnswer = "b", Question = "How many broomsticks are flown in a full game of Quidditch?" },
+                        new { ID = 8, Answer1 = "Liquorice Wand", Answer2 = "Sherbert Lemon", Answer3 = "Fizzing Whizzbee", Answer4 = "Cockroach Cluster", CorrectAnswer = "a", Question = "Which is not a password to get into Dumbledor's Office?" },
+                        new { ID = 9, Answer1 = "Hufflepuff", Answer2 = "Gryffindor", Answer3 = "Slytherin", Answer4 = "Ravenclaw", CorrectAnswer = "d", Question = "Which Hogwarts house is Terry Boot a member of?" },
+                        new { ID = 10, Answer1 = "Fluxweed", Answer2 = "Newt spleen", Answer3 = "A bit of whoever you wish toturn into", Answer4 = "Knotgrass", CorrectAnswer = "a", Question = "Which Polyjuice Potion ingredient must be acquired at the full moon?" },
+                        new { ID = 11, Answer1 = "Bird", Answer2 = "Serpent", Answer3 = "Fish", Answer4 = "Dog", CorrectAnswer = "b", Question = "What type of creature is an Ashwinder?" },
+                        new { ID = 12, Answer1 = "Lou Gehrig", Answer2 = "Pete Rose", Answer3 = "Cal Ripken Jr.", Answer4 = "Stan Musial", CorrectAnswer = "c", Question = "Which player holds the record for most consecutive games played in the MLB?" },
+                        new { ID = 13, Answer1 = "Indiana Pacers", Answer2 = "Mulwaukee Bucks", Answer3 = "Houston Rockets", Answer4 = "New York Knicks", CorrectAnswer = "a", Question = "Which of these teams has never won a championship?" },
+                        new { ID = 14, Answer1 = "Montreal Canadiens", Answer2 = "Green Bay Packers", Answer3 = "Boston Celtics", Answer4 = "New York Yankees", CorrectAnswer = "d", Question = "Which Big 4 team has the most championship wins with 27?" },
+                        new { ID = 15, Answer1 = "Hulk Hogan", Answer2 = "Andre the Giant", Answer3 = "Randy Savage", Answer4 = "Vince McMahon", CorrectAnswer = "b", Question = "Who was the first person inducted into the WWE Hall of Fame?" },
+                        new { ID = 16, Answer1 = "Pack-12", Answer2 = "SEC", Answer3 = "Big 10", Answer4 = "ACC", CorrectAnswer = "d", Question = "Which conference includes the Duke Blue Devils?" }
                     );
                 });
 #pragma warning restore 612, 618
